df0365a2f72de85dcccd9ac3e71ea411aca30eb1
sam-lewis <samlewis79@googlemail.com>
2016-06-06 12:57:11 +0100

    Create an example of elasticmq failing to create the configured queues

---
+++ src/test/java/com/example/ElasticMqTest.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import org.elasticmq.rest.sqs.SQSRestServer;
+import org.elasticmq.rest.sqs.SQSRestServerBuilder;
+import org.junit.Test;
+
+public class ElasticMqTest {
+
+	@Test
+	public void testQueueCreation() {
+
+		String queueName = "testQueue";
+
+		System.setProperty("config.resource", "custom.conf");
+		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "debug");
+
+		SQSRestServer server = SQSRestServerBuilder.start();
+		server.waitUntilStarted();
+
+		AmazonSQSClient client = new AmazonSQSClient(new BasicAWSCredentials("x", "x"));
+
+		client.setEndpoint("http://localhost:9324");
+
+//		Test passes when the queue is explicitly created
+//		client.createQueue(queueName);
+
+		String queueUrl = client.getQueueUrl(queueName).getQueueUrl();
+
+		client.sendMessage(queueUrl, "hello");
+
+		server.stopAndWait();
+	}
+}
